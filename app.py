8e9a879fd439a09b34a62fca853f71b0c0e01d5c
Chandan Sohi <91230351+sohic@users.noreply.github.com>
2024-05-09 19:00:26 -0700

    added ping call, added input validation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import re
 
 app = Flask(__name__)
 
@@ -6,9 +7,14 @@ app = Flask(__name__)
 def USDtoEuroRate():
     return 0.94  
 
-@app.route('/<usd>', methods=['GET'])
+def valid_input(usd):
+    valid_format = r'^(\d{1,3}(,\d{3})*|\d+)\.\d{2}$'
+    return bool(re.match(valid_format, usd))
+
+@app.route('/convert/<usd>', methods=['GET'])
 def convert_usd_to_euro(usd):
-    # usd = request.args.get('amount', default=1, type=float)
+    if not valid_input(usd):
+        return "Invalid input, please enter a valid dollar amount with a maximum of two decimal place values."
     exchangeRate = USDtoEuroRate()
     euros = float(usd) * exchangeRate
     return jsonify({
@@ -16,6 +22,11 @@ def convert_usd_to_euro(usd):
         'euros': round(euros, 2),
         'exchangeRate': exchangeRate
     })
+    
+@app.route('/ping')
+def ping():
+    return "pong"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8006)
