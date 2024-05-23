e3cde2650702b3a4a9fd8f9068eab12839c9be38
Chandan Sohi <91230351+sohic@users.noreply.github.com>
2024-05-22 22:26:14 -0700

    updated app.py

--- app.py
+++ app.py
@@ -8,23 +8,15 @@ def USDtoEuroRate():
     return 0.94  
 
 def valid_input(usd):
-    valid_format = r'^(\d+)(\.\d{2})?$'
-    
-    usd_str = str(usd)
-    match = re.match(valid_format, usd_str)
-    
-    if match:
-        decimal_part = match.group(2)
-        return bool(decimal_part)
-    else:
-        return False
+    valid_format = r'^(\d{1,3}(,\d{3})*|\d+)\.\d{2}$'
+    return bool(re.match(valid_format, usd))
 
 @app.route('/convert', methods=['GET'])
 def convert_usd_to_euro():
-    usd = request.args.get('usd', default=None, type=float)
+    usd = request.args.get('usd', default=None, type=str)
     if usd is None:
         return "Please enter a valid US Dollar value to convert to Euros.", 400, {'Content-Type': 'text/plain'}
-    if not valid_input(str(usd)):
+    if not valid_input(usd):
         return "Invalid input, please enter a valid dollar amount with exactly two decimal place values.", 400, {'Content-Type': 'text/plain'}
     exchangeRate = USDtoEuroRate()
     euros = float(usd) * exchangeRate
