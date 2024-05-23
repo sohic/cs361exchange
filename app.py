5c84ae5719d4e58dda4199ca8c867cdd698271d7
Chandan Sohi <91230351+sohic@users.noreply.github.com>
2024-05-22 22:17:29 -0700

    updated app.py

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ def USDtoEuroRate():
     return 0.94  
 
 def valid_input(usd):
-    valid_format = r'^(\d{1,3}(,\d{3})*|\d+)\.\d{2}$'
-    return bool(re.match(valid_format, usd))
+    valid_format = r'^(\d+)(\.\d{2})?$'
+    
+    usd_str = str(usd)
+    match = re.match(valid_format, usd_str)
+    
+    if match:
+        decimal_part = match.group(2)
+        return bool(decimal_part)
+    else:
+        return False
 
 @app.route('/convert', methods=['GET'])
 def convert_usd_to_euro():
